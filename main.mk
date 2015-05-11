bc3270594cc5744eba19e8d25b50291871ac6f81
Maarten Fonville <maarten.fonville@gmail.com>
2015-05-12 00:31:04 +0200

    initial scripts
    Change-Id: I197fef87dc3222d755cd937813ad51996608d83b

---
+++ main.mk
@@ -0,0 +1,45 @@
+#This file is part of The PA GApps script of @mfonville.
+#
+#    The PA GApps scripts are free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    These scripts are distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+TOPDIR := .
+BUILD_SYSTEM := $(TOPDIR)/scripts
+BUILD_GAPPS := $(BUILD_SYSTEM)/pa_gapps.sh
+API_LEVELS := 19 21 22
+PLATFORMS := arm arm64 x86
+LOWESTAPI_arm := 19
+LOWESTAPI_arm64 := 21
+LOWESTAPI_x86 := 19
+OUTDIR := $(TOPDIR)/out
+LOG_BUILD := /tmp/gapps_log
+
+define make-gapps
+#We first define 'all' so that this is the primary make target
+all:: $1
+
+#With this you can always call e.g. 'make arm-22' just to make only the arm packages
+#execute the build scripts with the platform and api as parameter, ensure minimum api
+#for the platform that is selected
+$1:	
+	$(platform = $(firstword $(subst -, ,$1)))
+	$(api = $(word 2, $(subst -, ,$1)))
+	@if [ "$(api)" -ge "$(LOWESTAPI_$(platform))" ] ; then\
+		echo "Generating PA GApps package for $(platform) with API level $(api)...";\
+		$(BUILD_GAPPS) $(platform) $(api) 2>&1 | tee $(LOG_BUILD);\
+	fi
+endef
+
+$(foreach platform,$(PLATFORMS),$(foreach api,$(API_LEVELS),$(eval $(call make-gapps,$(platform)-$(api)))))
+
+distclean :
+	@rm -fr $(OUTDIR)
+	@echo "$(tput setaf 2)Output removed! Ready for a clean build$(tput sgr 0)"
+
