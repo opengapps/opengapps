fb8df855d69ce11b920e9be1a3facd1d76a7f59e
mfonville <maarten.fonville@gmail.com>
2015-06-02 20:02:28 +0200

    Add success/error to download_sources, is used e.g. by the daily build script

--- download_sources.sh
+++ download_sources.sh
@@ -13,4 +13,12 @@
 #
 command -v git >/dev/null 2>&1 || { echo "git is required but it's not installed.  Aborting." >&2; exit 1; }
 git pull --recurse-submodules 
+if [ $? -eq 1 ]; then
+	echo "ERROR during git execution, aborted!"
+	exit 1
+fi
 git submodule update --init --remote
+if [ $? -eq 1 ]; then
+	echo "ERROR during git execution, aborted!"
+        exit 1
+fi
