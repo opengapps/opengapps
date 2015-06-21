4ed8969a24ceb43e1d5286fc667368241b2fd894
rapperskull <rapper.skull@hotmail.it>
2015-06-21 23:31:44 +0200

    Added some more duoble quotes

--- scripts/inc.updatebinary.sh
+++ scripts/inc.updatebinary.sh
@@ -872,11 +872,11 @@ ui_print "- Performing system space calculations";
 ui_print " ";
 
 # Perform calculations of device specific applications
-gms_size=$(unzip -lq $ZIP GMSCore/common/* GMSCore/${gms}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
-messenger_size=$(unzip -lq $ZIP Messenger/common/* Messenger/${msg}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
-playgames_size=$(unzip -lq $ZIP PlayGames/common/* PlayGames/${pg}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
-core_size=$(unzip -lq $ZIP Core/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
-keybd_lib_size=$(unzip -lq $ZIP Optional/keybd_lib/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+gms_size=$(unzip -lq "$ZIP" GMSCore/common/* GMSCore/${gms}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+messenger_size=$(unzip -lq "$ZIP" Messenger/common/* Messenger/${msg}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+playgames_size=$(unzip -lq "$ZIP" PlayGames/common/* PlayGames/${pg}/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+core_size=$(unzip -lq "$ZIP" Core/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+keybd_lib_size=$(unzip -lq "$ZIP" Optional/keybd_lib/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
 
 EOFILE
 if [ "$API" -gt "19" ]; then
@@ -949,7 +949,7 @@ log_sub "Install" "GMSCore²" $gms_size $post_install_size_kb;
 
 for gapp_name in $gapps_list; do
     if [ "$gapp_name" != "playgames" ] && [ "$gapp_name" != "messenger" ]; then
-        gapp_size_kb=$(unzip -lq $ZIP GApps/$gapp_name/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+        gapp_size_kb=$(unzip -lq "$ZIP" GApps/$gapp_name/* | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
     else
         eval "gapp_size_kb=\$${gapp_name}_size";
     fi
@@ -967,7 +967,7 @@ done;
 
 # Perform calculations of Core GApps Removals that will be performed after install
 for remove_folder in $gapps_remove_folder_list; do
-    core_size_kb=$(unzip -lq $ZIP "Core/required/${remove_folder#/system/}/*" | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
+    core_size_kb=$(unzip -lq "$ZIP" "Core/required/${remove_folder#/system/}/*" | tail -n1 | awk '{ size = $1 / 1024; printf "%.0f\n", size }');
     post_install_size_kb=$((post_install_size_kb + core_size_kb));
     if [ "$core_size_kb" -eq 0 ]; then
         printf "%7s | %26s | + %7s | %7d\n" "Remove" "${remove_folder#/system/*app/}°" unknown "$post_install_size_kb">> $calc_log;
