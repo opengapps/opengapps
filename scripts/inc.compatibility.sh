703cb4e7160262dd01410d61a24e63d28c4c82e6
mfonville <maarten.fonville@gmail.com>
2015-10-13 14:35:01 +0200

    That newline shit does not work in sh

--- scripts/inc.compatibility.sh
+++ scripts/inc.compatibility.sh
@@ -188,8 +188,10 @@ api22hack(){
 
     # On AOSP we only support Webview on 5.1+, stock Google ROMs support it on 5.0 too, but we're merging stock and fornexus
     case "$VARIANT" in # We prevent the removal of WebViewGoogle on packages smaller than stock
-      aroma|super|stock)  gappsstock="$gappsstock"$'\n'"webviewgoogle";
-                          stockremove="$stockremove"$'\n'"webviewstock";;
+      aroma|super|stock)  gappsstock="$gappsstock
+webviewgoogle";
+                          stockremove="$stockremove
+webviewstock";;
     esac
   fi
 }
