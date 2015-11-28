48abe2635fd49a972076f2d54ff095b5f1f90e70
mfonville <maarten.fonville@gmail.com>
2015-11-28 21:24:44 +0100

    We don't use Seed Setup after all, seems FM radio related

--- scripts/inc.sourceshelper.sh
+++ scripts/inc.sourceshelper.sh
@@ -63,7 +63,6 @@ getapkproperties(){
     "com.google.android.onetimeinitializer" |\
     "com.google.android.packageinstaller" |\
     "com.google.android.partnersetup" |\
-    "com.google.android.seedsetup" |\
     "com.google.android.setupwizard" |\
     "com.google.android.tungsten.setupwraith" |\
     "com.google.android.tag" |\
