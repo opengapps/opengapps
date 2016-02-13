043695b9ab469e906fea2991ce26bf6a99f5f311
mfonville <maarten.fonville@gmail.com>
2016-02-13 23:26:26 +0100

    Replace sed word replacement for in/exclude with awk

--- scripts/inc.updatebinary.sh
+++ scripts/inc.updatebinary.sh
@@ -456,10 +456,12 @@ if [ "$g_conf" ]; then
 
   # Create processed gapps-config with user comments stripped and user app removals removed and stored in variable for processing later
   g_conf="/tmp/proc_gconf";
-  sed -r -e 's/\<(in|ex)clude\>//gI' -e 's|#.*||g' -e 's/\r//g' -e 's/^[ \t]*//g' -e 's/[ \t]*$//g' -e '/^$/d' "$g_conf_orig" > "$g_conf"; # Remove in/exclude, strip comments+emptylines+spaces+tabs in gapps-config
+  awk '{IGNORECASE=1;gsub("(in|ex)clude", "");print}' "$g_conf_orig" > "$g_conf"; # drop in/exclude with awk
+  sed -i -e 's|#.*||g' -e 's/\r//g' -e 's/^[ \t]*//g' -e 's/[ \t]*$//g' -e '/^$/d' "$g_conf";
+  #TODO: We would prefer the line below instead of the 2 lines above, but sed-word replacement is broken in some recoveries
+  #sed -r -e 's/\<(in|ex)clude\>//gI' -e 's|#.*||g' -e 's/\r//g' -e 's/^[ \t]*//g' -e 's/[ \t]*$//g' -e '/^$/d' "$g_conf_orig" > "$g_conf"; # Remove in/exclude, strip comments+emptylines+spaces+tabs in gapps-config
   user_remove_list=$(awk -F "[()]" '{ for (i=2; i<NF; i+=2) print $i }' "$g_conf"); # Get users list of apk's to remove from gapps-config
-  sed -i s/'([^)]*)'/''/g "$g_conf"; # Remove all instances of user app removals (stuff between parentheses)
-  sed -i '/^$/d' "$g_conf"; # Remove all empty lines for cleaner appearance
+  sed -i -e s/'([^)]*)'/''/g -e '/^$/d'"$g_conf"; # Remove all instances of user app removals (stuff between parentheses) and empty lines
 else
   config_file="Not Used";
   g_conf="/tmp/proc_gconf";
