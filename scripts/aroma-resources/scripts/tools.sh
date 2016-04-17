1f814c805ae6b7abb3647b62993e329ef478d1e0
raulpetru <raulx222@gmail.com>
2016-04-17 23:24:23 +0200

    Fix AROMA loading of previous configs Thank you @mfonville for the debugging ;-)

--- scripts/aroma-resources/scripts/tools.sh
+++ scripts/aroma-resources/scripts/tools.sh
@@ -17,10 +17,10 @@ case "$1" in
 	cp /tmp/aroma/extra.prop /sdcard/Open-GApps/extra.prop
 	;;
 	load)
-	[ -f /sdcard/Open-GApps/aromagapps.prop ] && rm -f /tmp/aroma/aromagapps.prop && cp /sdcard/Open-GApps/aromagapps.prop /tmp/aroma/aromagapps.prop
-	[ -f /sdcard/Open-GApps/bypass.prop ] && rm -f /tmp/aroma/bypass.prop && cp /sdcard/Open-GApps/bypass.prop /tmp/aroma/bypass.prop
-	[ -f /sdcard/Open-GApps/rem.prop ] && rm -f /tmp/aroma/rem.prop && cp /sdcard/Open-GApps/rem.prop /tmp/aroma/rem.prop
-	[ -f /sdcard/Open-GApps/extra.prop ] && rm -f /tmp/aroma/extra.prop && cp /sdcard/Open-GApps/extra.prop /tmp/aroma/extra.prop
+	[ -f /sdcard/Open-GApps/aromagapps.prop ] && { rm -f /tmp/aroma/aromagapps.prop; cp /sdcard/Open-GApps/aromagapps.prop /tmp/aroma/aromagapps.prop;}
+	[ -f /sdcard/Open-GApps/bypass.prop ] && { rm -f /tmp/aroma/bypass.prop; cp /sdcard/Open-GApps/bypass.prop /tmp/aroma/bypass.prop;}
+	[ -f /sdcard/Open-GApps/rem.prop ] && { rm -f /tmp/aroma/rem.prop; cp /sdcard/Open-GApps/rem.prop /tmp/aroma/rem.prop;}
+	[ -f /sdcard/Open-GApps/extra.prop ] && { rm -f /tmp/aroma/extra.prop; cp /sdcard/Open-GApps/extra.prop /tmp/aroma/extra.prop;}
 	;;
 	reset)
 	rm -f /tmp/aroma/aromagapps.prop
