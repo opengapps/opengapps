0e5b9a047b4ae1e5910e458d308ae73cb68deb8b
mfonville <maarten.fonville@gmail.com>
2016-10-23 13:18:26 +0200

    Fix app_sizes bug that confused dpi with size

--- scripts/inc.installer.sh
+++ scripts/inc.installer.sh
@@ -837,7 +837,7 @@ get_appsize() {
     true*) odexsize="|odex";;
     *) odexsize="";;
   esac
-  appsize="$(cat $TMP/app_sizes.txt | grep -E "$app_name.*	($app_density|common$odexsize)" | awk 'BEGIN { app_size=0; } { folder_size=$3; app_size=app_size+folder_size; } END { printf app_size; }')"
+  appsize="$(cat $TMP/app_sizes.txt | grep -E "$app_name.*[[:blank:]]($app_density|common$odexsize)[[:blank:]]" | awk 'BEGIN { app_size=0; } { folder_size=$3; app_size=app_size+folder_size; } END { printf app_size; }')"
 }
 
 get_fallback_arch(){
