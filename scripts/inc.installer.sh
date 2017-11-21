f083993d90b4c1083e7510ef1aa7f2e0c3a50601
mfonville <maarten.fonville@gmail.com>
2017-11-21 20:36:51 +0100

    fix LD_LIBRARY_PATH thanks @osm0sis

--- scripts/inc.installer.sh
+++ scripts/inc.installer.sh
@@ -799,7 +799,10 @@ exxit() {
     cp -f "$gapps_removal_list" "$TMP/logs/gapps-remove_revised.txt"
     cp -f "$rec_cache_log" "$TMP/logs/Recovery_cache.log"
     cp -f "$rec_tmp_log" "$TMP/logs/Recovery_tmp.log"
-    env -u LD_LIBRARY_PATH logcat -d -f "$TMP/logs/logcat"
+    curLD="$LD_LIBRARY_PATH"
+    unset LD_LIBRARY_PATH
+    logcat -d -f "$TMP/logs/logcat"
+    export LD_LIBRARY_PATH="$curLD"
     cd "$TMP"
     tar -cz -f "$log_folder/open_gapps_debug_logs.tar.gz" logs/*
     cd /
