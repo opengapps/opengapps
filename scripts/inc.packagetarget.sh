a230ef965422a7abf88c70a65d5314f96a2ee59a
Nicholas Buse <nicholasbuse@gmail.com>
2018-04-17 09:11:14 -0500

    Revert "Switch from SignApk to ZipSigner 2.1 from @topjohnwu"
    This reverts commit 8f6e6fa9e8806c791a88f678c2ca77b23663bd50.
    
    NOTE: There is an issue with this commit with the OpenGApps automated build. It will be re-enabled after the issue is addressed.

--- scripts/inc.packagetarget.sh
+++ scripts/inc.packagetarget.sh
@@ -271,17 +271,17 @@ signzip() {
   fi
 
   if [ -z "$CERTIFICATEFILE" ] || [ ! -e "$CERTIFICATEFILE" ]; then
-    unset CERTIFICATEFILE
+    CERTIFICATEFILE="$CERTIFICATES/testkey.x509.pem"
   else
     echo "INFO: using $CERTIFICATEFILE as certificate file"
   fi
   if [ -z "$KEYFILE" ] || [ ! -e "$KEYFILE" ]; then
-    unset KEYFILE
+    KEYFILE="$CERTIFICATES/testkey.pk8"
   else
     echo "INFO: using $KEYFILE as cryptographic key file"
   fi
 
-  if java -jar "$SCRIPTS/zipsigner-resources/zipsigner-"*.jar $CERTIFICATEFILE $KEYFILE "$unsignedzip" "$signedzip"; then #if signing did succeed
+  if java -XX:+UseCompressedOops -Xms2g -Xmx2g -Djava.library.path="$SCRIPTS/signapk-resources-$(uname)/" -jar "$SCRIPTS/signapk.jar" -w "$CERTIFICATEFILE" "$KEYFILE" "$unsignedzip" "$signedzip"; then #if signing did succeed
     rm "$unsignedzip"
   else
     rm "$signedzip"
