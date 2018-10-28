0d4197685ede1990e54488c314fe7941ca58d1e4
Makornthawat Emery <makorn645@users.noreply.github.com>
2018-10-29 05:42:13 +0700

    [1/2] Drop deprecated news widget app (#682)

--- scripts/inc.buildtarget.sh
+++ scripts/inc.buildtarget.sh
@@ -59,7 +59,6 @@ keep
 movies
 music
 newsstand
-newswidget
 playgames
 sheets
 slides
@@ -345,7 +344,6 @@ get_package_info(){
     moviesvrmode)             packagetype="GApps"; packagename="com.google.android.videos.vrmode"; packagetarget="app/Videos";;
     music)                    packagetype="GApps"; packagename="com.google.android.music"; packagetarget="app/Music2";;
     newsstand)                packagetype="GApps"; packagename="com.google.android.apps.magazines"; packagetarget="app/Newsstand";;
-    newswidget)               packagetype="GApps"; packagename="com.google.android.apps.genie.geniewidget"; packagetarget="app/PrebuiltNewsWeather";;
     packageinstallergoogle)   packagetype="GApps"; packagename="com.google.android.packageinstaller"; packagetarget="priv-app/GooglePackageInstaller";;
     pixelicons)               packagetype="GApps"; packagename="com.google.android.nexusicons"; packagetarget="app/NexusLauncherIcons";;
     pixellauncher)            packagetype="GApps"; packagename="com.google.android.apps.nexuslauncher"; packagetarget="priv-app/NexusLauncherPrebuilt";;
