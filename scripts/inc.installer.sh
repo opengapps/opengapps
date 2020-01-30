7af571c3733763c0160c11dd48bd5a5d08f4964f
osm0sis <osm0sis@outlook.com>
2020-01-30 11:40:45 -0400

    installer: mount fixes for TWRP SAR(s) and Lineage 16+17 Recoveries TWRP now has 3(!) different competing implementations of SAR currently in use by maintainers: 1) /system as mountpoint in ramdisk/fstab and ANDROID_ROOT=/system, and possible symlink from /system_root to /system 2) /system_root as mountpoint in ramdisk/fstab, ANDROID_ROOT=/system_root, and possible symlink from /system to /system_root/system 3) /system_root as mountpoint in ramdisk/fstab, ANDROID_ROOT=/system, and possible symlink from /system to /system_root/system
    Lineage 16 has: /system_root as mountpoint in ramdisk, neither /system nor /system_root in fstab (it has / instead, which is dangerous), ANDROID_ROOT=/system, and a possible symlink from /system to /system_root/system
    Lineage 17 has: /system mountpoint in ramdisk, neither /system nor /system_root in fstab (it has / instead, which is dangerous), ANDROID_ROOT=/system, and no /system_root symlink
    
    TWRP is working on standardizing their fragmented SAR support, and Lineage is also looking into resolving some issues with their AOSP Q setup.
    
    These changes should work across the board since Open GApps is using its own busybox enviornment for install, and should automatically work with TWRP's final SAR setup, but will benefit from an update once Lineage works theirs out, since it will likely be very different due to AOSP Q recovery.

--- scripts/inc.installer.sh
+++ scripts/inc.installer.sh
@@ -940,7 +940,9 @@ get_file_prop() {
   grep -m1 "^$2=" "$1" | cut -d= -f2
 }
 
-is_mounted() { mount | grep -q " $1 "; }
+set_progress() { echo "set_progress $1" > "$OUTFD"; }
+
+ui_print() { echo -e "ui_print $1\nui_print" > "$OUTFD"; }
 
 setup_mountpoint() {
   test -L $1 && mv -f $1 ${1}_link
@@ -950,15 +952,98 @@ setup_mountpoint() {
   fi
 }
 
-set_progress() { echo "set_progress $1" > "$OUTFD"; }
+is_mounted() { mount | grep -q " $1 "; }
 
-ui_print() {
-  echo "ui_print $1" > "$OUTFD"
-  echo "ui_print" > "$OUTFD"
+mount_apex() {
+  test -d /system/apex || return 1
+  local apex dest loop minorx num
+  setup_mountpoint /apex
+  test -e /dev/block/loop1 && minorx=$(ls -l /dev/block/loop1 | awk '{ print $6 }') || minorx=1
+  num=0
+  for apex in /system/apex/*; do
+    dest=/apex/$(basename $apex .apex)
+    test "$dest" == /apex/com.android.runtime.release && dest=/apex/com.android.runtime
+    mkdir -p $dest
+    case $apex in
+      *.apex)
+        unzip -qo $apex apex_payload.img -d /apex
+        mv -f /apex/apex_payload.img $dest.img
+        mount -t ext4 -o ro,noatime $dest.img $dest 2>/dev/null
+        if [ $? != 0 ]; then
+          while [ $num -lt 64 ]; do
+            loop=/dev/block/loop$num
+            (mknod $loop b 7 $((num * minorx))
+            losetup $loop $dest.img) 2>/dev/null
+            num=$((num + 1))
+            losetup $loop | grep -q $dest.img && break
+          done
+          mount -t ext4 -o ro,loop,noatime $loop $dest
+          if [ $? != 0 ]; then
+            losetup -d $loop 2>/dev/null
+          fi
+        fi
+      ;;
+      *) mount -o bind $apex $dest;;
+    esac
+  done
+  export ANDROID_RUNTIME_ROOT=/apex/com.android.runtime
+  export ANDROID_TZDATA_ROOT=/apex/com.android.tzdata
+  export BOOTCLASSPATH=/apex/com.android.runtime/javalib/core-oj.jar:/apex/com.android.runtime/javalib/core-libart.jar:/apex/com.android.runtime/javalib/okhttp.jar:/apex/com.android.runtime/javalib/bouncycastle.jar:/apex/com.android.runtime/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/android.test.base.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.media/javalib/updatable-media.jar
+}
+
+umount_apex() {
+  test -d /apex || return 1
+  local dest loop
+  for dest in $(find /apex -type d -mindepth 1 -maxdepth 1); do
+    if [ -f $dest.img ]; then
+      loop=$(mount | grep $dest | cut -d" " -f1)
+    fi
+    (umount -l $dest
+    losetup -d $loop) 2>/dev/null
+  done
+  rm -rf /apex 2>/dev/null
+  unset ANDROID_RUNTIME_ROOT ANDROID_TZDATA_ROOT BOOTCLASSPATH
+}
+
+mount_all() {
+  if ! is_mounted /data; then
+    mount /data
+    UMOUNT_DATA=1
+  fi
+  (mount /cache
+  mount -o ro -t auto /persist
+  mount -o ro -t auto /vendor) 2>/dev/null
+  setup_mountpoint $ANDROID_ROOT
+  if ! is_mounted $ANDROID_ROOT; then
+    mount -o ro -t auto $ANDROID_ROOT 2>/dev/null
+  fi
+  case $ANDROID_ROOT in
+    /system_root) setup_mountpoint /system;;
+    /system)
+      if ! is_mounted /system && ! is_mounted /system_root; then
+        setup_mountpoint /system_root
+        mount -o ro -t auto /system_root
+      elif [ -f /system/system/build.prop ]; then
+        setup_mountpoint /system_root
+        mount --move /system /system_root
+      fi
+      if [ $? != 0 ]; then
+        umount /system
+        umount -l /system 2>/dev/null
+        test -e /dev/block/bootdevice/by-name/system || local slot=$(getprop ro.boot.slot_suffix 2>/dev/null)
+        mount -o ro -t auto /dev/block/bootdevice/by-name/system$slot /system_root
+      fi
+    ;;
+  esac
+  if is_mounted /system_root; then
+    mount -o bind /system_root/system /system
+  fi
+  mount_apex
 }
 
 umount_all() {
-  (umount /system
+  (umount_apex
+  umount /system
   umount -l /system
   if [ -e /system_root ]; then
     umount /system_root
@@ -968,7 +1053,7 @@ umount_all() {
     umount $p
     umount -l $p
   done
-  if [ "$umount_data" ]; then
+  if [ "$UMOUNT_DATA" ]; then
     umount /data
     umount -l /data
   fi) 2>/dev/null
@@ -1013,37 +1098,7 @@ set_progress 0.01
 test "$ANDROID_ROOT" || ANDROID_ROOT=/system
 mount -o bind /dev/urandom /dev/random
 umount_all
-
-setup_mountpoint $ANDROID_ROOT
-if ! is_mounted $ANDROID_ROOT; then
-  mount -o ro -t auto $ANDROID_ROOT
-fi
-case $ANDROID_ROOT in
-  /system_root) setup_mountpoint /system;;
-  /system)
-    if [ -f /system/system/build.prop ]; then
-      setup_mountpoint /system_root
-      mount --move /system /system_root
-      if [ $? != 0 ]; then
-        umount /system
-        umount -l /system 2>/dev/null
-        mount -o ro -t auto /dev/block/bootdevice/by-name/system /system_root
-      fi
-    fi
-  ;;
-esac
-if is_mounted /system_root; then
-  mount -o bind /system_root/system /system
-fi
-
-for p in "/cache" "/persist" "/vendor"; do
-  mount -o ro -t auto $p 2>/dev/null
-done
-if ! is_mounted /data; then
-  mount /data
-  umount_data=1
-fi
-
+mount_all
 mount -o rw,remount -t auto /system
 mount -o rw,remount -t auto /vendor 2>/dev/null
 ui_print " "
@@ -1177,8 +1232,12 @@ exxit() {
   set_progress 1.0
   ui_print "- Unmounting partitions"
   umount_all
-  (mv -f /system_link /system
-  mv -f /system_root_link /system_root
+  (for dir in /apex /system /system_root; do
+    if [ -L "${dir}_link" ]; then
+      rmdir $dir
+      mv -f ${dir}_link $dir
+    fi
+  done
   umount -l /dev/random) 2>/dev/null
   ui_print " "
   exit "$1"
