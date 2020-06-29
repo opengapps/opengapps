45e09d4c66f432f67afcfd75e1a41a41f950c925
Ilya Danilkin <nezorflame@gmail.com>
2020-06-29 15:29:43 +0300

    Fix overlays
    - remove overlays before installing new ones
    - set proper SELinux context for overlay files
    
    Signed-off-by: Ilya Danilkin <nezorflame@gmail.com>

--- scripts/inc.buildtarget.sh
+++ scripts/inc.buildtarget.sh
@@ -279,6 +279,7 @@ get_package_info(){
     actionsservices)          packagetype="GApps"; packagename="com.google.android.as"; packagetarget="priv-app/MatchmakerPrebuilt"
                               if [ "$API" -ge "28" ]; then  # Add an overlay for Android 9.0+
                                 packagefiles="product/overlay/ActionsServicesOverlay.apk"
+                                packagegappsremove="product/overlay/ActionsServicesOverlay.apk vendor/overlay/ActionsServicesOverlay.apk"
                               fi;;
     androidauto)              packagetype="GApps"; packagename="com.google.android.projection.gearhead"; packagetarget="app/AndroidAutoPrebuilt";;
     batteryusage)             packagetype="GApps"; packagename="com.google.android.apps.turbo"; packagetarget="priv-app/Turbo";;
@@ -310,6 +311,7 @@ get_package_info(){
     dialergoogle)             packagetype="GApps"; packagename="com.google.android.dialer"; packagetarget="priv-app/GoogleDialer"
                               if [ "$API" -ge "28" ]; then  # Add an overlay for Android 9.0+
                                 packagefiles="product/overlay/DefaultDialerOverlay.apk product/overlay/PhoneOverlay.apk product/overlay/TelecomOverlay.apk"
+                                packagegappsremove="product/overlay/DefaultDialerOverlay.apk product/overlay/PhoneOverlay.apk product/overlay/TelecomOverlay.apk vendor/overlay/DefaultDialerOverlay.apk vendor/overlay/PhoneOverlay.apk vendor/overlay/TelecomOverlay.apk"
                               fi;;
     dmagent)                  packagetype="GApps"; packagename="com.google.android.apps.enterprise.dmagent"; packagetarget="app/DMAgent";;
     docs)                     packagetype="GApps"; packagename="com.google.android.apps.docs.editors.docs"; packagetarget="app/EditorsDocs";;
@@ -351,6 +353,7 @@ get_package_info(){
     pixellauncher)            packagetype="GApps"; packagename="com.google.android.apps.nexuslauncher"; packagetarget="priv-app/NexusLauncherPrebuilt"
                               if [ "$API" -ge "28" ]; then  # Add an overlay for Android 9.0+
                                 packagefiles="product/overlay/PixelLauncherOverlay.apk"
+                                packagegappsremove="product/overlay/PixelLauncherOverlay.apk vendor/overlay/PixelLauncherOverlay.apk"
                               fi;;
     photos)                   packagetype="GApps"; packagename="com.google.android.apps.photos"; packagetarget="app/Photos";;
     photosvrmode)             packagetype="GApps"; packagename="com.google.android.apps.photos.vrmode"; packagetarget="app/Photos";;
@@ -378,6 +381,7 @@ get_package_info(){
     wellbeing)                packagetype="GApps"; packagename="com.google.android.apps.wellbeing"; packagetarget="priv-app/WellbeingPrebuilt"
                               if [ "$API" -ge "28" ]; then  # Add an overlay for Android 9.0+
                                 packagefiles="product/overlay/WellbeingOverlay.apk"
+                                packagegappsremove="product/overlay/WellbeingOverlay.apk vendor/overlay/WellbeingOverlay.apk"
                               fi;;
     youtube)                  packagetype="GApps"; packagename="com.google.android.youtube"; packagetarget="app/YouTube";;
     zhuyin)                   packagetype="GApps"; packagename="com.google.android.apps.inputmethod.zhuyin"; packagetarget="app/GoogleZhuyinIME";;  # ZhuyinIME exists in some ROMs
