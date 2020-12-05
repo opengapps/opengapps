a06c236a29bd7b653a0f63ed6e1c8fefc3650b14
mfonville <maarten.fonville@gmail.com>
2020-12-05 20:36:22 +0100

    Update APKtool to 2.5.0

--- add_sourceapp.sh
+++ add_sourceapp.sh
@@ -17,7 +17,7 @@ CACHE="$TOP/cache"
 SOURCES="$TOP/sources"
 SCRIPTS="$TOP/scripts"
 CERTIFICATES="$SCRIPTS/certificates"
-APKTOOL="$SCRIPTS/apktool-resources/apktool_2.4.1.jar"
+APKTOOL="$SCRIPTS/apktool-resources/apktool_2.5.0.jar"
 # shellcheck source=scripts/inc.tools.sh
 . "$SCRIPTS/inc.tools.sh"
 # shellcheck source=scripts/inc.compatibility.sh
