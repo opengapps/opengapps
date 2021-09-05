ea5190935aa078178d81762f39042f7e53aadcc0
mfonville <maarten.fonville@gmail.com>
2021-09-05 11:03:14 +0200

    Update APKtool to 2.6.0

--- add_sourceapp.sh
+++ add_sourceapp.sh
@@ -17,7 +17,7 @@ CACHE="$TOP/cache"
 SOURCES="$TOP/sources"
 SCRIPTS="$TOP/scripts"
 CERTIFICATES="$SCRIPTS/certificates"
-APKTOOL="$SCRIPTS/apktool-resources/apktool_2.5.0.jar"
+APKTOOL="$SCRIPTS/apktool-resources/apktool_2.6.0.jar"
 # shellcheck source=scripts/inc.tools.sh
 . "$SCRIPTS/inc.tools.sh"
 # shellcheck source=scripts/inc.compatibility.sh
